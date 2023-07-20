4f6ddee40ad06ab03bde193f65d0e93499f5ac72
mifuyutsuki <84283107+mifuyutsuki@users.noreply.github.com>
2023-07-20 16:50:02 +0700

    Add function to connect via SQLAlchemy

--- geoid_db/connect.py
+++ geoid_db/connect.py
@@ -1,12 +1,46 @@
-import mysql.connector
-import getpass
+from sqlalchemy import create_engine
+
+import tomllib
+from urllib.parse import quote_plus
 
 
-def connect(user: str, host: str='localhost', db: str='geoid'):
-  print(f'Connecting to MySQL database {db} at {host} as {user}')
-  return mysql.connector.connect(
-    user=user,
-    password=getpass.getpass(),
-    host=host,
-    database=db
-  )
+ENGINE = '%(use_backend)s+%(use_driver)s://%(username)s:%(password)s@%(host)s:%(port)s/%(dbname)s'
+
+
+def connect():
+  """
+  Connect to database using information given by config.toml.
+
+  Returns:
+      New database engine instance
+  """
+
+  try:
+    config = _load_config()
+  except FileNotFoundError:
+    print(f'Could not find "config.toml" in active directory')
+    return
+
+  config_db = config['database']
+  for key in config_db.keys():
+    if type(config_db[key]) is str:
+      config_db[key] = quote_plus(config_db[key])
+
+  username  = config_db['username']
+  host      = config_db['host']
+  port      = config_db['port']
+  print(f'Connecting to database: {username}@{host}:{port}')
+
+  return create_engine(ENGINE % config_db)
+
+
+def _load_config():
+  """
+  Load configuration file config.toml in active (`__main__`) directory.
+
+  Returns:
+      Dict parsed from config.toml
+  """
+
+  with open('config.toml', 'rb') as f:
+    return tomllib.load(f)
