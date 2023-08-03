10f00f669d2345e80e92be32606898fc80a6780f
mifuyutsuki <84283107+mifuyutsuki@users.noreply.github.com>
2023-08-03 17:18:48 +0700

    Add license and copyright notice

--- geoid_db/schema/consts.py
+++ geoid_db/schema/consts.py
@@ -1,6 +1,27 @@
+# Copyright (c) 2023 mifuyutsuki
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
 class Keys:
-  #: Used in import and export. Internally, the name given by the attribute
-  #: name in lowercase is used (postal_code instead of kode_pos)
+  #: Used in import (TODO: and export). Internally, the name given by the
+  #: attribute name in lowercase is used (postal_code instead of kode_pos)
 
   #: Query keys
   QUERY_ID            = 'query_id'
